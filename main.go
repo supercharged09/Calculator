bc5a2e61894315ba31ad33e5c983728862668615
Vladimir Savranchuk <miststyle@inbox.ru>
2025-09-13 19:34:49 +0300

    calculator test

--- main.go
+++ main.go
@@ -25,3 +25,31 @@ func main() {
 		fmt.Print("invalid operator/number")
 	}
 }
+
+/* package main
+
+import "fmt"
+
+func main() {
+	var a, b float64
+	var operator string
+	fmt.Println("Введите первое число:")
+	fmt.Scan(&a)
+	fmt.Println("Введите оператор (+,-,*,/")
+	fmt.Scan(&operator)
+	fmt.Println("Введите второе число:")
+	fmt.Scan(&b)
+	fmt.Print(a, operator, b, " = ")
+
+	if operator == "+" {
+		fmt.Print(a + b)
+	} else if operator == "-" {
+		fmt.Print(a - b)
+	} else if operator == "*" {
+		fmt.Print(a * b)
+	} else if operator == "/" {
+		fmt.Print(a / b)
+	} else {
+		fmt.Print("invalid operator/number")
+	}
+} */
